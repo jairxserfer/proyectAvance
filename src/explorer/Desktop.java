8466b8116ea099d3e065d087fe83a7efc8de2370
Punkike <aguinaga.victor@gmail.com>
2020-12-04 16:49:05 -0500

    iconos y tamaños

--- src/explorer/Desktop.java
+++ src/explorer/Desktop.java
@@ -18,7 +18,9 @@ public class Desktop {
     private JPanel jBackground;
 
 
-    ImageIcon iconobtn = new ImageIcon(new ImageIcon("src/images/youtube-app-icon-png-3.jpg").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
+    ImageIcon iconobtnYT = new ImageIcon(new ImageIcon("src/images/youtube-app-icon-png-3.jpg").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
+    ImageIcon iconobtnFE = new ImageIcon(new ImageIcon("src/images/File-Explorer-fluent-icon.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
+    ImageIcon iconobtnCALC = new ImageIcon(new ImageIcon("src/images/calc.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Desktop");
@@ -36,13 +38,32 @@ public class Desktop {
     }
 
     public void botones(){
-        youtubeButton.setIcon(iconobtn);
+        //BOTON YOUTUBE
+        youtubeButton.setIcon(iconobtnYT);
         youtubeButton.setIconTextGap(2);
         youtubeButton.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         youtubeButton.setVerticalAlignment(javax.swing.SwingConstants.CENTER);
         youtubeButton.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
         youtubeButton.setVerticalTextPosition(javax.swing.SwingConstants.CENTER);
         youtubeButton.setPreferredSize(new Dimension(20,20));
+
+        //BOTON EXPLORADOR
+        fileExplorerButton.setIcon(iconobtnFE);
+        fileExplorerButton.setIconTextGap(2);
+        fileExplorerButton.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        fileExplorerButton.setVerticalAlignment(javax.swing.SwingConstants.CENTER);
+        fileExplorerButton.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
+        fileExplorerButton.setVerticalTextPosition(javax.swing.SwingConstants.CENTER);
+        fileExplorerButton.setPreferredSize(new Dimension(20,20));
+
+        //BOTON CALCULADORA
+        calcButton.setIcon(iconobtnCALC);
+        calcButton.setIconTextGap(2);
+        calcButton.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        calcButton.setVerticalAlignment(javax.swing.SwingConstants.CENTER);
+        calcButton.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
+        calcButton.setVerticalTextPosition(javax.swing.SwingConstants.CENTER);
+        calcButton.setPreferredSize(new Dimension(20,20));
     }
 
 
