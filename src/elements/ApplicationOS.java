2c14680ca928275d60c77e07af014fc176727947
Punkike <aguinaga.victor@gmail.com>
2020-12-09 18:21:04 -0500

    size

--- src/elements/ApplicationOS.java
+++ src/elements/ApplicationOS.java
@@ -1,11 +1,11 @@
 package elements;
 
 public class ApplicationOS extends DirectoryOS{
-    public ApplicationOS(String name, String location, String size, String son, String parent) {
+    public ApplicationOS(String name, String location, int size, String son, String parent) {
         super(name, location, size, son, parent);
     }
 
-    public ApplicationOS(String name, String location, String size) {
+    public ApplicationOS(String name, String location, int size) {
         super(name, location, size);
     }
 
