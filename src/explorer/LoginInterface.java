dfd4fb0679684a9276f39d9758f13b4bdf6bd196
Punkike <aguinaga.victor@gmail.com>
2020-12-11 19:23:18 -0500

    Login_interface

--- src/explorer/LoginInterface.java
+++ src/explorer/LoginInterface.java
@@ -10,6 +10,10 @@ public class LoginInterface {
     private JPasswordField passwordTF;
     private JButton logInButton;
     private JPanel jPanel;
+    private JLabel javaOSLabel;
+    private JButton soLogoButton;
+
+    ImageIcon iconobtnSO = new ImageIcon(new ImageIcon("src/images/so-logo.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("LoginInterface");
@@ -18,5 +22,29 @@ public class LoginInterface {
         frame.pack();
         frame.setSize(new Dimension(800,600));
         frame.setVisible(true);
+    }
+
+    public LoginInterface(){
+        boton();
+    }
+
+    public void boton() {
+        //BOTON YOUTUBE
+        soLogoButton.setIcon(iconobtnSO);
+        soLogoButton.setIconTextGap(2);
+        soLogoButton.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        soLogoButton.setVerticalAlignment(javax.swing.SwingConstants.CENTER);
+        soLogoButton.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
+        soLogoButton.setVerticalTextPosition(javax.swing.SwingConstants.CENTER);
+        soLogoButton.setPreferredSize(new Dimension(20, 20));
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        userLabel.setPreferredSize(new Dimension(-1,-1));
+        userLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+
+        passLabel.setPreferredSize(new Dimension(-1,-1));
+        passLabel.setHorizontalAlignment(SwingConstants.RIGHT);
     }
 }
